80e1783f9dfdd04a112f86c0dfedd98d16f5fd14
Harrison <li.harrison.h@gmail.com>
2017-09-12 12:02:14 -0700

    got the window_transform_series method working

--- my_answers.py
+++ my_answers.py
@@ -10,11 +10,21 @@ import keras
 # and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_series(series, window_size):
     # containers for input/output pairs
-    X = []
-    y = []
-
+    # I create a 2d array as X. each element of outer list is a subsequence of inputs
+    # with length window_size. can only create length(original series) - window_size subsequences
+    # though. Thus if length series was 10 and window_size was 2 then can only have 8 input
+    # output pairs because the 9th input window would have valid input but no output to associate it with
+    # note X is capitalized because it is a matrix
+    X = [series[i:i+window_size] for i in range(len(series)- window_size)]
+    # then populate the output scalars. gets the element right after each end of an input window
+    # note: no need for +1 because remember the 'i+window_size'-th element in series is actually
+    # one after series[i:i+window_size] because of non-inclusive slicing
+    y = [series[i+window_size] for i in range(len(series) - window_size)]
     # reshape each 
     X = np.asarray(X)
+    # np.shape(X) gets the shape of the nd array X. (ie if it is a 3d array with 3 rows, 4 columns and 2 other dimension)
+    # then it will output a tuple of (3,4,2). So doing [0:2] just gets the first 2 dimenions (outer most dimensions)
+    # and then we assign th at shape to our empty nd array X so that now it has that shape
     X.shape = (np.shape(X)[0:2])
     y = np.asarray(y)
     y.shape = (len(y),1)
