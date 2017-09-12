728f94fc9007cac4c37630550277dcfa1524b329
Harrison <li.harrison.h@gmail.com>
2017-09-12 13:56:49 -0700

    Got window transform text to work

--- my_answers.py
+++ my_answers.py
@@ -8,18 +8,19 @@ import keras
 
 # TODO: fill out the function below that transforms the input series 
 # and window-size into a set of input/output pairs for use with our RNN model
-def window_transform_series(series, window_size):
+# added an optional step_size=1 argument to help work with window_transform_text
+def window_transform_series(series, window_size, step_size=1):
     # containers for input/output pairs
     # I create a 2d array as X. each element of outer list is a subsequence of inputs
     # with length window_size. can only create length(original series) - window_size subsequences
     # though. Thus if length series was 10 and window_size was 2 then can only have 8 input
     # output pairs because the 9th input window would have valid input but no output to associate it with
     # note X is capitalized because it is a matrix
-    X = [series[i:i+window_size] for i in range(len(series)- window_size)]
+    X = [series[i:i+window_size] for i in range(0, len(series)- window_size, step_size)]
     # then populate the output scalars. gets the element right after each end of an input window
     # note: no need for +1 because remember the 'i+window_size'-th element in series is actually
     # one after series[i:i+window_size] because of non-inclusive slicing
-    y = [series[i+window_size] for i in range(len(series) - window_size)]
+    y = [series[i+window_size] for i in range(0, len(series) - window_size, step_size)]
     # reshape each 
     X = np.asarray(X)
     # np.shape(X) gets the shape of the nd array X. (ie if it is a 3d array with 3 rows, 4 columns and 2 other dimension)
@@ -63,10 +64,18 @@ def cleaned_text(text):
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
 def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
-    inputs = []
-    outputs = []
-
-    return inputs,outputs
+    # leveraging window_transform_series
+    inputs, outputs = window_transform_series(text, window_size, step_size)
+    # make inputs into a list since window_transform_series outputs an nd-array
+    inputs = list(inputs)
+    # then outputs the shape is different so I can't just cast to a list or else 
+    # it's like a list of np arrays. So, need to go into each np array and then
+    # append each character element individually to get a list of chararacters
+    new_output = []
+    for output in outputs:
+        for element in output:
+            new_output.append(element)
+    return inputs, new_output
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
